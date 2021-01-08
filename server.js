43c89660c9abd2cd8cfab4c06dac0be233815215
Heidi Sairanen <hasair88@gmail.com>
2021-01-09 00:44:18 +0200

    Connection to the database

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // Import frameworks and modules
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Initialize the framework
 const app = express();
+
+// Connect to the database
+mongoose.connect(process.env.DATABASE_URL,{useUnifiedTopology: true,useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to the database.'))
 
 // Create route handlers
 
